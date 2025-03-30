c72dc652f50c2f9e3a3e2df9a493d3f0f02e44c7
yusufdinc974 <dinc.yusuf974@gmail.com>
2025-03-30 21:17:33 +0300

    i am fixing something though i don't know what is going on version 2

--- src/sections/skills/SkillsSection.jsx
+++ src/sections/skills/SkillsSection.jsx
@@ -161,20 +161,7 @@ const SkillsSection = () => {
             </span>
             Categories
           </button>
-          <button 
-            key="graph-button"
-            className={`toggle-btn ${activeView === 'graph' ? 'active' : ''}`}
-            onClick={() => setActiveView('graph')}
-          >
-            <span className="btn-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <circle cx="12" cy="12" r="10"></circle>
-                <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-                <path d="M2 12h20"></path>
-              </svg>
-            </span>
-            Circuit Graph
-          </button>
+          
         </div>
 
         {/* Categories view */}
